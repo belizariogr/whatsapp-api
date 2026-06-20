8962b387113b78f6499d895fdad394fc7df53a12
Belizário Ribeiro <belizariogr@gmail.com>
2026-06-20 13:04:51 -0300

    Enhance error handling and logging for application startup
    - Improved error messages in `src/index.ts` to provide more context during application startup failures.
    - Updated the `bootstrap` function to ensure that all startup errors are logged and handled gracefully.
    - Refined the application initialization process to catch and report errors more effectively, improving overall reliability.

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env /bin/bash
+
+cd $(dirname $0)
+BASE_DIR=$PWD
+
+#START
+cd $BASE_DIR
+
+
+BACKEND_HOST="bg-api-us"
+BACKEND_PATH="whatsapp"
+DIFF_SRC_PATH=$BASE_DIR/
+
+echo ""
+echo "Comparing backend files..."
+echo ""
+
+echo "Creating a backup on the remote server..."
+ssh $BACKEND_HOST "rm -rf backups/$BACKEND_PATH\2 && mv backups/$BACKEND_PATH\1 backups/$BACKEND_PATH\2"
+ssh $BACKEND_HOST "rsync -aq --mkpath $BACKEND_PATH/* backups/$BACKEND_PATH\1 --exclude uploads --exclude logs --exclude docs --exclude node_modules"
+
+echo ""
+echo "Uploading backend files..."
+
+rsync --info=progress2 --del -rt "$DIFF_SRC_PATH" $BACKEND_HOST:$BACKEND_PATH --exclude-from=$DIFF_SRC_PATH.gitignore
+
+echo "Running migrations..."
+ssh $BACKEND_HOST "source .bashrc && cd $BACKEND_PATH && bun migrate"
+
+echo "Stopping docker container..."
+ssh $BACKEND_HOST "docker stop $IMAGE_NAME"
+
+ssh $BACKEND_HOST "cd $BACKEND_PATH && bun install"
+
+echo "Starting docker container..."
+ssh $BACKEND_HOST "cd $BACKEND_PATH && docker compose up -d"
